2912b6064a67b0b3ccdf1918247f4c01884f20b3
darknessxk <me@darknessxk.com>
2019-06-20 14:49:41 -0300

    WPF UI Data Converter (IMultiValueConverter)

---
+++ SteamAccountToolkit/Classes/WPF/AddUserMultiConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace SteamAccountToolkit.Classes.WPF
+{
+    public class AddUserMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(values[0] is PasswordBox userPass))
+                return null;
+            if (!(values[1] is PasswordBox authSecret))
+                return null;
+
+            return new[]
+            {
+                userPass,
+                authSecret
+            };
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SteamAccountToolkit/Classes/WPF/GenericMultiConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SteamAccountToolkit.Classes.WPF
+{
+    class GenericMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return values.Clone();
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
